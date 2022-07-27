ae34d94c790e5c1027f725761c9c68e17544718c
Alan Orth <alan.orth@gmail.com>
2022-07-27 11:09:57 +0300

    Add basic cgspace theme
    For now this is just a copy of the default DSpace 7.3 dspace theme
    with a CGIAR logo.
    
    See: https://wiki.lyrasis.org/display/DSDOC7x/User+Interface+Customization

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+import { EagerThemeModule as DSpaceEagerThemeModule } from './cgspace/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 
 /**
