0f291d66a3af90d5f4791c54a788ba15565d55f6
Alan Orth <alan.orth@gmail.com>
2023-06-22 20:51:16 +0300

    src: add a custom ORCID identifier component
    Add a custom ORCID identifier component for the simple item page.
    For now this is implemented directly in the main src/app because
    we can't do it as a themed component yet.
    
    As for the implementation, I don't know Angular at all so this is
    probably a very naive implementation that I will have to re-visit
    later.

---
+++ src/app/item-page/simple/field-components/specific-field/orcid/item-page-orcid-field.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input } from '@angular/core';
+
+import { Item } from '../../../../../core/shared/item.model';
+import { ItemPageFieldComponent } from '../item-page-field.component';
+
+@Component({
+  selector: 'ds-item-page-orcid-field',
+  templateUrl: './item-page-orcid-field.component.html'
+})
+/**
+ * This component renders an ORCID identifier as a name, icon, and link.
+ * It expects 4 parameters: The item, a separator, the metadata keys and an i18n key
+ */
+export class ItemPageOrcidFieldComponent extends ItemPageFieldComponent {
+
+  /**
+   * The item to display metadata for
+   */
+  @Input() item: Item;
+
+  /**
+   * Separator string between multiple values of the metadata fields defined
+   * @type {string}
+   */
+  @Input() separator: string;
+
+  /**
+   * Fields (schema.element.qualifier) used to render their values.
+   */
+  @Input() fields: string[];
+
+  /**
+   * Label i18n key for the rendered metadata
+   */
+  @Input() label: string;
+
+}
