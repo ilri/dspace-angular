22fcc39bd86ef5c9924efc91d02bb67eaf974af8
Alan Orth <alan.orth@gmail.com>
2023-07-03 22:24:22 +0300

    src/app: add license component to simple item page
    Use similar logic of the SDG component to display an appropriate
    icon for Creative Commons licenses. For now this only checks for
    well-formed SPDX license strings like "CC-BY-4.0", and not "CC-BY".
    
    If an item does not have a Creative Commons license then the comp-
    onent will print the license value.
    
    See: https://creativecommons.org/about/downloads/

---
+++ src/app/item-page/simple/field-components/specific-field/license/item-page-license-field.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input } from '@angular/core';
+
+import { Item } from '../../../../../core/shared/item.model';
+import { ItemPageFieldComponent } from '../item-page-field.component';
+
+@Component({
+  selector: 'ds-item-page-license-field',
+  templateUrl: './item-page-license-field.component.html'
+})
+/**
+ * This component renders the license section.
+ * It expects 4 parameters: The item, a separator, the metadata keys and an i18n key
+ */
+export class ItemPageLicenseFieldComponent extends ItemPageFieldComponent {
+
+  /**
+   * The item to display metadata for
+   */
+  @Input() item: Item;
+
+  /**
+   * Fields (schema.element.qualifier) used to render their values.
+   */
+  @Input() fields: string[];
+
+  /**
+   * Label i18n key for the rendered metadata
+   */
+  @Input() label: string;
+
+  /**
+   * Helper function to return the appropriate CC license icon. Licenses can be
+   * versions 1.0, 2.0, 2.5, 3.0, or 4.0 for most (I don't try to match those
+   * without versions).
+   * @type {string}
+   */
+  parseLicense(license: string) {
+    if (license.match(/CC-BY-[1234]/i) != null) {
+      return "by.svg";
+    }
+    else if (license.match(/CC-BY-SA-[1234]/) != null) {
+      return "by-sa.svg";
+    }
+    else if (license.match(/CC-BY-ND-[1234]/) != null) {
+      return "by-nd.svg";
+    }
+    else if (license.match(/CC-BY-NC-[1234]/) != null) {
+      return "by-nc.svg";
+    }
+    else if (license.match(/CC-BY-NC-SA-[1234]/) != null) {
+      return "by-nc-sa.svg";
+    }
+    else if (license.match(/CC-BY-NC-ND-[1234]/) != null) {
+      return "by-nc-nd.svg";
+    }
+    else if (license.match(/CC0-1.0/) != null) {
+      return "cc-zero.svg";
+    }
+    // Should never get here, we will see. ;)
+    else {
+      return "boo";
+    }
+  }
+}
