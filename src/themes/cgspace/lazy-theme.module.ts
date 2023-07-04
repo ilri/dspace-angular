e176979ad91d4d76964614397d0173134688afed
Alan Orth <alan.orth@gmail.com>
2023-07-04 16:01:15 +0300

    src/themes/cgspace: add themed file section component
    Add a themed file section component that displays the bitstream's
    description in lieu of the name if it is present. As a compromise
    between DSpace 7's default and the old XMLUI behavior, I also add
    the file's extension as a cue to the user (via a helper function).
    
    If the bitstream does not have a description it uses the same dis-
    play as the default file selection component.

--- src/themes/cgspace/lazy-theme.module.ts
+++ src/themes/cgspace/lazy-theme.module.ts
@@ -55,6 +55,7 @@ import {
 } from '../../app/shared/resource-policies/resource-policies.module';
 import { ComcolModule } from '../../app/shared/comcol/comcol.module';
 import { RootModule } from '../../app/root.module';
+import { FileSectionComponent } from './app/item-page/simple/field-components/file-section/file-section.component';
 import { BrowseByPageModule } from '../../app/browse-by/browse-by-page.module';
 import { ResultsBackButtonModule } from '../../app/shared/results-back-button/results-back-button.module';
 import { SharedBrowseByModule } from '../../app/shared/browse-by/shared-browse-by.module';
@@ -66,6 +67,7 @@ import { ThumbnailComponent } from './app/thumbnail/thumbnail.component';
 const DECLARATIONS = [
   ItemPageComponent,
   ThumbnailComponent,
+  FileSectionComponent,
 ];
 
 @NgModule({
