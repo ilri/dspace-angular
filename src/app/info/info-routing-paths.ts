c438d6ff145d046ec3f0938a83767e7dc5c7161e
Alan Orth <alan.orth@gmail.com>
2023-09-17 17:12:45 +0000

    src/app/info: add a CGSpace about page
    For now this is in the main src because it is implemented in the
    info component similarly to the privacy and other pages and it is
    not possible to do in the theme.

--- src/app/info/info-routing-paths.ts
+++ src/app/info/info-routing-paths.ts
@@ -6,6 +6,9 @@ export const FEEDBACK_PATH = 'feedback';
 export const COAR_NOTIFY_SUPPORT = 'coar-notify-support';
 export const ACCESSIBILITY_SETTINGS_PATH = 'accessibility';
 
+// CGSpace about page
+export const ABOUT_PATH = 'about';
+
 export function getEndUserAgreementPath() {
   return getSubPath(END_USER_AGREEMENT_PATH);
 }
@@ -24,6 +27,10 @@ export function getCOARNotifySupportPath(): string {
 
 export function getAccessibilitySettingsPath() {
   return getSubPath(ACCESSIBILITY_SETTINGS_PATH);
+}
+
+export function getAboutPath() {
+    return getSubPath(ABOUT_PATH);
 }
 
 function getSubPath(path: string) {
