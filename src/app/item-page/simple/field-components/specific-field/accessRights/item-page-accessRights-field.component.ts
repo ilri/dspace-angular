5ddd5828ec48c15335d8aa975c51f5c31d46547b
Alan Orth <alan.orth@gmail.com>
2023-12-16 23:05:32 +0300

    src/app: Minor adjustment to CGSpace accessRights component
    To keep the heading size consistent we need to mimic the style and
    structure of the metadata-field-wrapper component. Part of this was
    to remove unneccessary duplication of some simple-view classes.

--- src/app/item-page/simple/field-components/specific-field/accessRights/item-page-accessRights-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/accessRights/item-page-accessRights-field.component.ts
@@ -5,6 +5,8 @@ import { ItemPageFieldComponent } from '../item-page-field.component';
 
 @Component({
   selector: 'ds-item-page-accessRights-field',
+  // Mimic style from metadata-field-wrapper component since we can't use it here
+  styleUrls: ['../../../../../shared/metadata-field-wrapper/metadata-field-wrapper.component.scss'],
   templateUrl: './item-page-accessRights-field.component.html'
 })
 /**
