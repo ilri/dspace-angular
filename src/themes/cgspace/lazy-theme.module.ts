094f1d5290e005e53520d0139df928b3a1f67935
Alan Orth <alan.orth@gmail.com>
2024-01-08 16:13:26 +0300

    src/themes/cgspace: add search form to collection pages

--- src/themes/cgspace/lazy-theme.module.ts
+++ src/themes/cgspace/lazy-theme.module.ts
@@ -63,11 +63,14 @@ import { ResultsBackButtonModule } from '../../app/shared/results-back-button/re
 import { SharedBrowseByModule } from '../../app/shared/browse-by/shared-browse-by.module';
 import { ItemVersionsModule } from '../../app/item-page/versions/item-versions.module';
 import { ItemSharedModule } from 'src/app/item-page/item-shared.module';
+import { CollectionPageComponent } from './app/collection-page/collection-page.component';
 import { ItemPageComponent } from './app/item-page/simple/item-page.component';
 import { ThumbnailComponent } from './app/thumbnail/thumbnail.component';
+import { DsoPageModule } from '../../app/shared/dso-page/dso-page.module';
 
 const DECLARATIONS = [
   HomePageComponent,
+  CollectionPageComponent,
   ItemPageComponent,
   ThumbnailComponent,
   FileSectionComponent,
@@ -127,6 +130,7 @@ const DECLARATIONS = [
     FormsModule,
     ResourcePoliciesModule,
     ComcolModule,
+    DsoPageModule,
   ],
   declarations: DECLARATIONS,
 })
