a1631a5b923b69b30aa5480c7e2a419025c9aa55
Alan Orth <alan.orth@gmail.com>
2024-01-26 14:25:41 +0300

    src/app/item-page: move custom styles to components
    We shouldn't be defining component-specific styles globally.

--- src/themes/cgspace/app/footer/footer.component.ts
+++ src/themes/cgspace/app/footer/footer.component.ts
@@ -3,8 +3,7 @@ import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.
 
 @Component({
   selector: 'ds-footer',
-  // styleUrls: ['./footer.component.scss'],
-  styleUrls: ['../../../../app/footer/footer.component.scss'],
+  styleUrls: ['./footer.component.scss'],
   templateUrl: './footer.component.html'
 })
 export class FooterComponent extends BaseComponent {
