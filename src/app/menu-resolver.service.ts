0c843b098cba8f736883b31db22600a06118a321
Alan Orth <alan.orth@gmail.com>
2025-01-22 16:49:50 +0300

    src/app: use en dash in menu

--- src/app/menu-resolver.service.ts
+++ src/app/menu-resolver.service.ts
@@ -193,7 +193,7 @@ export class MenuResolverService  {
             visible: true,
             model: {
               type: MenuItemType.LINK,
-              text: 'CGIAR research programs and platforms (2011-2021)',
+              text: 'CGIAR research programs and platforms (2011–2021)',
               link: '/communities/d09e6100-df60-4280-9ff3-f3fea5ea4e6b'
             } as LinkMenuItemModel
           });
